22f561de5c7a10e6e26070255a90359e88855276
artdrawing <artdrawing116@gmail.com>
2016-07-30 13:40:27 +0300

    Create art.js

---
+++ art.js
@@ -0,0 +1,12 @@
+Enter file contents hereBarva function (coda) 
+  { 
+  document.getElementById (& quot; vzorec & quot;). bgcolor = coda; 
+  document.hcc.barva.value = koda.toUpperCase (); 
+  document.hcc.barva.select (); 
+  } 
+  BarvaDruga function (coda) 
+  { 
+  document.getElementById (& quot; vzorec2 & quot;). bgcolor = coda; 
+  document.hcc.Barva2.value = koda.toUpperCase (); 
+  document.hcc.Barva2.select (); 
+  }
